6cbd0afed9a3c69eb044f7de6ce9385675709236
Sandikha Rahardi <sandikha.rahardi@gmail.com>
2021-01-11 17:18:43 +0700

    hello world

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Expenses App'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text('Hello Wolrd !'),
+        ),
+      ),
+    );
+  }
+}

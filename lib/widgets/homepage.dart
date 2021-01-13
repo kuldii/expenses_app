82d67f00a6c6e05658590fbef00cb88a7edc3f09
Sandikha Rahardi <sandikha.rahardi@gmail.com>
2021-01-13 22:54:58 +0700

    membuat init data list transaction

--- lib/widgets/homepage.dart
+++ lib/widgets/homepage.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
+import '../models/transaction.dart';
 import '../widgets/listtransaction.dart';
 import '../widgets/newtransaction.dart';
 import '../widgets/chart.dart';
 
 class HomePage extends StatelessWidget {
+  final List<Transaction> dataTransaction = [
+    Transaction(
+      title: "Kopi",
+      amount: 1.99,
+    ),
+    Transaction(
+      title: "Gula",
+      amount: 2.99,
+    ),
+    Transaction(
+      title: "Teh",
+      amount: 1.99,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(

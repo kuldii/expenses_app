1fe8c5ed18761a8d25dc512ffd6ef632c106a3db
Sandikha Rahardi <sandikha.rahardi@gmail.com>
2021-01-13 22:59:02 +0700

    merapikan model transaction dengan menambah required

--- lib/widgets/homepage.dart
+++ lib/widgets/homepage.dart
@@ -7,16 +7,22 @@ import '../widgets/chart.dart';
 class HomePage extends StatelessWidget {
   final List<Transaction> dataTransaction = [
     Transaction(
+      id: "tx1",
       title: "Kopi",
       amount: 1.99,
+      date: DateTime.now(),
     ),
     Transaction(
+      id: "tx2",
       title: "Gula",
       amount: 2.99,
+      date: DateTime.now(),
     ),
     Transaction(
+      id: "tx3",
       title: "Teh",
       amount: 1.99,
+      date: DateTime.now(),
     ),
   ];
 

465cd68dd1d067b234ccf3ead02c897727d455e2
Sandikha Rahardi <sandikha.rahardi@gmail.com>
2021-01-14 16:09:11 +0700

    memformat date dengan intl

--- lib/widgets/listtransaction.dart
+++ lib/widgets/listtransaction.dart
@@ -1,5 +1,6 @@
 import '../models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class ListTransactions extends StatefulWidget {
   final List<Transaction> _dataTransaction;
@@ -16,52 +17,58 @@ class _ListTransactionsState extends State<ListTransactions> {
     return Column(
       children: widget._dataTransaction.map((value) {
         return Card(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              // amount
-              Container(
-                margin: EdgeInsets.fromLTRB(20, 5, 0, 5),
-                padding: EdgeInsets.symmetric(
-                  horizontal: 10,
-                  vertical: 5,
-                ),
-                decoration: BoxDecoration(
-                  color: Theme.of(context).primaryColor,
-                  borderRadius: BorderRadius.circular(7),
-                ),
-                child: Text(
-                  "\$ " + value.amount.toStringAsPrecision(4),
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                // amount
+                Container(
+                  margin: EdgeInsets.fromLTRB(20, 5, 0, 5),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 10,
+                    vertical: 5,
                   ),
-                ),
-              ),
-              // detail
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    value.title,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(7),
+                  ),
+                  child: Text(
+                    "\$ " + value.amount.toStringAsPrecision(4),
                     style: TextStyle(
+                      color: Colors.white,
                       fontSize: 16,
-                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                  Text(
-                    value.date.toString(),
-                  ),
-                ],
-              ),
-              // delete
-              IconButton(
-                icon: Icon(
-                  Icons.delete,
                 ),
-                onPressed: () {},
-              ),
-            ],
+                // detail
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      value.title,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      DateFormat().format(value.date),
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+                // delete
+                IconButton(
+                  icon: Icon(
+                    Icons.delete,
+                  ),
+                  onPressed: () {},
+                ),
+              ],
+            ),
           ),
         );
       }).toList(),

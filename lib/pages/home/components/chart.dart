27cd8ddcde30927a99640ce012dc43bc05556542
Sandikha Rahardi <sandikha.rahardi@gmail.com>
2021-01-22 21:27:34 +0700

    memperbaiki tampilan chart container

--- lib/pages/home/components/chart.dart
+++ lib/pages/home/components/chart.dart
@@ -6,93 +6,101 @@ class ChartWidget extends StatelessWidget {
     return Card(
       elevation: 5,
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Text(
                 "\$99.99",
               ),
               Container(
-                height: 10,
-                width: 5,
+                height: 100,
+                width: 20,
                 color: Colors.amber,
               ),
               Text("M"),
             ],
           ),
           Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Text(
                 "\$99.99",
               ),
               Container(
-                height: 10,
-                width: 5,
+                height: 100,
+                width: 20,
                 color: Colors.amber,
               ),
               Text("T"),
             ],
           ),
           Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Text(
                 "\$99.99",
               ),
               Container(
-                height: 10,
-                width: 5,
+                height: 100,
+                width: 20,
                 color: Colors.amber,
               ),
               Text("W"),
             ],
           ),
           Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Text(
                 "\$99.99",
               ),
               Container(
-                height: 10,
-                width: 5,
+                height: 100,
+                width: 20,
                 color: Colors.amber,
               ),
               Text("T"),
             ],
           ),
           Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Text(
                 "\$99.99",
               ),
               Container(
-                height: 10,
-                width: 5,
+                height: 100,
+                width: 20,
                 color: Colors.amber,
               ),
               Text("F"),
             ],
           ),
           Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Text(
                 "\$99.99",
               ),
               Container(
-                height: 10,
-                width: 5,
+                height: 100,
+                width: 20,
                 color: Colors.amber,
               ),
               Text("S"),
             ],
           ),
           Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Text(
                 "\$99.99",
               ),
               Container(
-                height: 10,
-                width: 5,
+                height: 100,
+                width: 20,
                 color: Colors.amber,
               ),
               Text("S"),
